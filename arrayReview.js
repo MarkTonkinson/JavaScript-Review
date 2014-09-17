5ecf98634c002c71a06cbe79f372e62dad034147
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-17 15:21:43 -0600

    first commit after completing array and object reviews

--- arrayReview.js
+++ arrayReview.js
@@ -2,8 +2,10 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
-
+var last = function(arr){
+	return arr.slice(arr.length-1);
+}
+alert(last(threeItems));
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -11,8 +13,10 @@ var threeItems = [1,2,3];
 //Fix 'dicedArray' below so that it's an array full of integers from 0-10
 var dicedArray = [0,1,4,5,7,8,10];
 
-  //code here
-
+dicedArray.splice(2,0,2,3);
+dicedArray.splice(6,0,6);
+dicedArray.splice(8,0,9);
+console.log(dicedArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -21,9 +25,13 @@ var dicedArray = [0,1,4,5,7,8,10];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
-
-
+for(var i = 0; i < evenArray.length; i++){
+	if(evenArray[i] % 2 !== 0){
+		evenArray.splice(i,1);
+        i--;
+	}
+}
+console.log(evenArray);
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
@@ -34,7 +42,17 @@ var getRandomArbitrary = function() {
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+function checkArray(){
+	var num = getRandomArbitrary();
+	var index = randomArray.indexOf(num);
+    if(index !== -1){
+    	alert(true);
+    }
+    else {
+    	alert(false);
+    }
+}
+checkArray();
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -45,7 +63,8 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+second = first.slice(0,5)
+second.splice(5,0,6,7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -59,24 +78,65 @@ alert(second) //[1,2,3,4,5,6,7];
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
 
-  //code here
-
-
+var longest = function(str){
+	var strArray = str.split(" ");
+	var longestWord = "";
+	for (var i = 0; i < strArray.length; i++){
+		var wordLength = strArray[i].length;
+		if(wordLength > longestWord.length) {
+			longestWord = strArray[i];
+		}			
+	}
+	return(longestWord + " is the longest word");
+}
+alert(longest(sentence));
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
 //write a function called capitalize that takes in the myPoem variable and capitalizes every word 
-var myPoem = 'What is a jQuery but a misunderstood object?'
+var myPoem = 'What is a jQuery but a misunderstood object?';
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+var capitalize = function(strVar){
+var individualWords = strVar.split(" ");
+var capWord = [];
+	for (var i = 0; i < individualWords.length; i++){
+		capWord[i]  =  individualWords[i].charAt(0).toUpperCase() + individualWords.splice(1,i.length) + individualWords[i].slice(1);
+	}
+	return capWord.join(" ");
+};
+
+alert(capitalize(myPoem));
 
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
-
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+var vowelArr = ['a', 'e', 'i', 'o', 'u'];
+
+var vowelCounter = function (str) {
+    var strArr = str.toLowerCase().split('');
+    var vowArr = [];
+    for (var i = 0; i < strArr.length; i++) {
+        for (var j = 0; j < vowelArr.length; j++) {
+            if (strArr[i] === vowelArr[j]) {
+                vowArr.push(strArr[i]);
+            }
+        }
+    }
+    return vowArr.length + " vowels!";
+};
+
+alert(vowelCounter(theOdyssey));
+//should return 25
+
+
+
+
+
+
+
