a2d560bd74e3a513ffe36a70cd660c38aff9a8a6
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-17 20:09:29 -0600

    Getting closer to finished

--- callbackReview.js
+++ callbackReview.js
@@ -3,9 +3,17 @@
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var first = function(arr, cb){
+  cb(arr[0]);
+}
+
+
 first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+  console.log('The first name in names is ' + firstName)
 });
+
+
 
 
 
@@ -13,9 +21,13 @@ first(names, function(firstName){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
-
-
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var last = function(arr, cb){
+  var lastName = arr.length - 1;
+  cb(arr[lastName]);
+}
+
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
@@ -25,15 +37,22 @@ last(names, function(lastName){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
-
+//**********************COME BACK TO THIS ONE!!!!!!!!!!!!!!!!!****************************
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-contains('Colt', function(yes){
+
+var contains(str, cb){
+  var yes;
+  if (str === "Colt") {
+    var inArr = yes;
+    cb(inArr);
+  }
+}
+
+contains(names, 'Colt', function(yes){
   if(yes){
-    'Colt is in the array';
+    alert('Colt is in the array');
   } else {
-    'Colt is not in the list';
+    alert('Colt is not in the list');
   }
 });
 
@@ -42,10 +61,16 @@ contains('Colt', function(yes){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
-
+//**************COME BACK TO THIS ONE*******************
 var numbers = [1,2,3,4,5];
+
+var map = function (arr, cb) {
+    var num = [];
+    for (var i = 0; i < arr.length; i++) {
+        num.push(arr[i]);
+    }
+    cb(num);
+};
 //Produces a new array of values by mapping each value in list through a transformation function
 map(, function(num){
   return num * 2; //returns an array of [2,4,6,8,10]
@@ -60,6 +85,19 @@ map(, function(num){
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var uniq = function(arr, cb){
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[i] === arr[j]){
+        arr.splice(i,1);
+        i--;
+      }
+    }
+  }
+  return cb(arr);
+}
+
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
@@ -73,8 +111,15 @@ uniq(names, function(uniqArr){
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var each = function(arr, cb){
+  for(var i = 0; i < arr.length; i++) {
+     cb(arr[i],arr.indexOf(arr[i]));
+  }
+}
+
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
@@ -105,6 +150,17 @@ var users = [
     address: '192 East 32 North'
   },
 ];
+
+var getUserById = function (id, cb){
+  for(var i = 0; i < users.length; i++){
+    if(users[i].id === id){
+      cb(users[i])
+    }
+  }
+}
+
+
+
 getUserById('16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
 });
@@ -115,10 +171,25 @@ getUserById('16t', function(user){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
-
+//***********COME BACK TO ME!!!!!*****************
 
 //Looks through each value in the list, returning the first one that passes a truth test 
 var numbers  = [1, 2, 3, 4, 5, 6];
+
+var find = function(arr, cb){
+  for(var i = 0; i < arr.length; i++){
+   debugger;
+    if(arr[i] % 2 === 0){
+      var num = arr[i];
+      break;
+    }
+  }
+  cb(num);
+}
+
 find(numbers, function(num){ 
   return num % 2 == 0; //should return 2
 })
+
+
+
--- closureReview.js
+++ closureReview.js
@@ -9,4 +9,27 @@ var callFriend = function(){
 //Above your given a callFriend function, that returns another function. 
 //Do what you need with the code above in order to console.log 'Calling Jake at 435-215-9248'
 
+var callFriend = function(){
+  var friend = 'Jake';
+  var number;
+  var callF;
+  var callF = function(number){
+    return 'Calling ' + friend + ' at ' + number;
+    var number = "435-215-9248"
 
+  }
+  return callF;
+};
+console.log(callFriend())
+
+Some Progress
+var callFriend = function(){
+  var friend = 'Jake';
+  var number;
+  var callF = function(number){
+    number= "435-215-9248";
+    return number;
+  }
+   return 'Calling ' + friend + ' at ' + number;
+};
+console.log(callFriend())
